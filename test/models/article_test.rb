0d4b2882b53999e71d2c26fed3d78479ed0cd47d
Hector Rojas <rojasmery4@protonmail.com>
2022-12-17 20:24:53 -0300

    implementación test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "no se debería guardar con nombre invalido" do
+    articles(:article_invalido_uno, :article_invalido_dos, :article_invalido_tres,
+      :article_invalido_cuatro).each do |d|
+      res = d.valid?
+      assert_equal false, res, "DPNombre no está validando el dato del nombre #{d.nombre}"
+    end
+  end
 end
